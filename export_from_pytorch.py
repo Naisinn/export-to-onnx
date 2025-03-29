b670c50368f93df0dfec1f2f2a523ae17222f777
Naisinn <62951486+Naisinn@users.noreply.github.com>
2025-03-29 15:33:36 +0900

    エラー　変換対象の.ptファイルのパスを実行後に入力

--- export_from_pytorch.py
+++ export_from_pytorch.py
@@ -5,7 +5,10 @@ from torch.autograd import Variable
 import torchvision
 from torchvision import datasets, models, transforms
 
-vgg16 = models.vgg16(pretrained=True)
+# 変換対象の.ptファイルのパスを実行後に入力
+pt_path = input("変換対象の.ptファイルのパスを入力してください: ")
+vgg16 = torch.load(pt_path)
+vgg16.eval()
 
 x = Variable(torch.randn(1, 3, 224, 224))
 torch.onnx.export(vgg16, x, 'vgg16_pytorch.onnx', verbose=True, opset_version=10)
